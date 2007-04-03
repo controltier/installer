4315e08ee93eaca0e01b7a866d7a8d38515aec84
Greg Schueler <greg@dtosolutions.com>
2007-04-03 18:57:25 +0000

    fix jar dependency references: regexp 1.5, ant-contrib 1.0b3

--- installer/src/sh/install.sh
+++ installer/src/sh/install.sh
@@ -37,7 +37,7 @@ fi
 PATH=$JAVA_HOME/bin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH
 
-$JAVA_HOME/bin/java -Dant.home=$ANT_HOME -cp lib/installer.jar:$ANT_HOME/lib/ant.jar:$ANT_HOME/lib/ant-launcher.jar:$ANT_HOME/lib/regexp-1.2.jar:$ANT_HOME/lib/ant-apache-regexp.jar:$ANT_HOME/lib/ant-contrib-1.0b1.jar:$ANT_HOME/lib/ant-nodeps.jar:$ANT_HOME/lib/ant-trax.jar \
+$JAVA_HOME/bin/java -Dant.home=$ANT_HOME -cp lib/installer.jar:$ANT_HOME/lib/ant.jar:$ANT_HOME/lib/ant-launcher.jar:$ANT_HOME/lib/regexp-1.5.jar:$ANT_HOME/lib/ant-apache-regexp.jar:$ANT_HOME/lib/ant-contrib-1.0b3.jar:$ANT_HOME/lib/ant-nodeps.jar:$ANT_HOME/lib/ant-trax.jar \
      com.controltier.install.CTierInstaller $*
 
 if [ $? -ne 0 ]; then
