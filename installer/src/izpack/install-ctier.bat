8e5f0cf701af6d3167464468ed880d53ca6e8a92
Greg Schueler <greg@dtosolutions.com>
2008-12-18 01:00:29 +0000

    set CTIER_ROOT environment variable before calling install.bat
    git-svn-id: file:///Users/noahcampbell/Source/ctrep/branches/controltier-3-2-dev@8947 d4a9e8c4-ac4d-4c38-8d82-82c8b6c5cbc9

--- installer/src/izpack/install-ctier.bat
+++ installer/src/izpack/install-ctier.bat
@@ -3,6 +3,7 @@ set JAVA_HOME=$JDKPath
 @rem Check if JAVA_HOME is set to a valid JDK
 @rem @@@TODO
 
+set CTIER_ROOT=$INSTALL_PATH
 cd "$INSTALL_PATH\pkgs\ControlTier-${CTVERSION}"
 call INSTALL.BAT
 exit 0
