e4b4527b3e28581e4edace8e34468502a403c9ad
Greg Schueler <greg@dtosolutions.com>
2009-09-02 21:02:20 +0000

    update project.xml definitions to latest
    git-svn-id: file:///Users/noahcampbell/Source/ctrep/branches/controltier-3-4-support@9454 d4a9e8c4-ac4d-4c38-8d82-82c8b6c5cbc9

--- buildall.sh
+++ buildall.sh
@@ -16,10 +16,10 @@ if [ ! -f $HOME/.ssh/id_dsa.pub ] ; then
     exit 1
 fi
 
-CTLVERS=1.4.7
-CTIERVERS=3.4.7
-RCVERS=0.8.7
-JCVERS=1.4.7
+CTLVERS=1.4.8
+CTIERVERS=3.4.8
+RCVERS=0.8.8
+JCVERS=1.4.8
 
 
 CTLSVNROOT="https://ctl-dispatch.svn.sourceforge.net/svnroot/ctl-dispatch"
