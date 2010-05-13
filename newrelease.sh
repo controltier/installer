a8ca797673114fd97a63448b434b65b0e31b738c
Greg Schueler <greg@dtosolutions.com>
2010-05-13 17:26:56 +0000

    Update release/tag scripts for 3.5 sourcebase
    git-svn-id: file:///Users/noahcampbell/Source/ctrep/branches/controltier-3-5-dev@9971 d4a9e8c4-ac4d-4c38-8d82-82c8b6c5cbc9

--- newrelease.sh
+++ newrelease.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ]  ; then
-    echo "usage: newrelease <ctl-version> <ctl-path> <ctier-version> <ct-path> <jc-version> <jc-path> <rc-version> [-status | -commit]"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
+    echo "usage: newrelease <ct-version> <ctl-path> <ct-path> [-status | -diff | -commit]"
     exit 1
 fi
 
-CTLVER=$1
+CTVER=$1
 CTLPATH=$2
-CTVER=$3
-CTPATH=$4
-JCVER=$5
-JCPATH=$6
-RCVER=$7
-RCPATH=$CTPATH/reportcenter
+CTPATH=$3
+CCPATH=$CTPATH/ctl-center
+CTLVER=$CTVER
 
-if [ "$8" == "-status" ] ; then
+if [ "$4" == "-status" ] ; then
     svn stat $CTLPATH/project.xml $CTLPATH/bundle/project.xml $CTLPATH/version.properties
-    svn stat $CTPATH/version.properties $CTPATH/common/project.xml $CTPATH/commander/project.xml $CTPATH/workbench/project.xml $CTPATH/installer/project.xml $CTPATH/ctbuild/objects/project.xml $CTPATH/buildall.sh $CTPATH/ctbuild/build.properties $CTPATH/ctbuild/jobs/jobs.xml
-    svn stat $JCPATH/application.properties $JCPATH/grails-app/i18n/messages.properties $JCPATH/etc/install.xml $JCPATH/version.properties
-    svn stat $RCPATH/application.properties $RCPATH/grails-app/i18n/messages.properties $RCPATH/etc/install.xml $RCPATH/version.properties
+    svn stat $CTPATH/version.properties $CTPATH/common/project.xml $CTPATH/commander/project.xml $CTPATH/workbench/project.xml $CTPATH/installer/project.xml  $CTPATH/buildall.sh $CTPATH/ctbuild/build.properties $CTPATH/ctbuild/jobs/jobs.xml
+    svn stat $CCPATH/application.properties $CCPATH/grails-app/i18n/messages.properties $CCPATH/etc/install.xml $CCPATH/version.properties
+    exit 0
+fi
+if [ "$4" == "-diff" ] ; then
+    svn diff $CTLPATH/project.xml $CTLPATH/bundle/project.xml $CTLPATH/version.properties
+    svn diff $CTPATH/version.properties $CTPATH/common/project.xml $CTPATH/commander/project.xml $CTPATH/workbench/project.xml $CTPATH/installer/project.xml  $CTPATH/buildall.sh $CTPATH/ctbuild/build.properties $CTPATH/ctbuild/jobs/jobs.xml
+    svn diff $CCPATH/application.properties $CCPATH/grails-app/i18n/messages.properties $CCPATH/etc/install.xml $CCPATH/version.properties
     exit 0
 fi
 
@@ -60,41 +62,25 @@ perl  -0777 -i'.orig' -p -e "s#(<id>ctl</id>\s*)<version>.*?</version>#\$1<versi
 perl  -0777 -i'.orig' -p -e "s#(<id>itnav</id>\s*)<version>.*?</version>#\$1<version>$CTVER</version>#sg" $CTPATH/installer/project.xml
 perl  -0777 -i'.orig' -p -e "s#(<id>commander-extension</id>\s*)<version>.*?</version>#\$1<version>$CTVER</version>#sg" $CTPATH/installer/project.xml
 perl  -0777 -i'.orig' -p -e "s#(<id>ctier-examples</id>\s*)<version>.*?</version>#\$1<version>$CTVER</version>#sg" $CTPATH/installer/project.xml
-perl  -0777 -i'.orig' -p -e "s#(<id>jobcenter</id>\s*)<version>.*?</version>#\$1<version>$JCVER</version>#sg" $CTPATH/installer/project.xml
-perl  -0777 -i'.orig' -p -e "s#(<id>reportcenter</id>\s*)<version>.*?</version>#\$1<version>$RCVER</version>#sg" $CTPATH/installer/project.xml
+perl  -0777 -i'.orig' -p -e "s#(<id>ctlcenter</id>\s*)<version>.*?</version>#\$1<version>$CTVER</version>#sg" $CTPATH/installer/project.xml
 
-# update jobcenter version
+# update ctlcenter version
 
-perl  -i'.orig' -p -e "s#^version\.number\s*=.*\$#version.number=$JCVER#" $JCPATH/version.properties
-perl  -i'.orig' -p -e "s#^app\.version=.*\$#app.version=$JCVER#" $JCPATH/application.properties
-perl  -i'.orig' -p -e "s#^jobcenter\.version\.num=.*\$#jobcenter.version.num=$JCVER#" $JCPATH/grails-app/i18n/messages.properties
-perl  -i'.orig' -p -e "s#<property\s+name=\"jobcenter.version\"\s+value=\".*?\"#<property name=\"jobcenter.version\" value=\"$JCVER\"#" $JCPATH/etc/install.xml
-perl  -i'.orig' -p -e "s#<property\s+name=\"commander.version\"\s+value=\".*?\"#<property name=\"commander.version\" value=\"$CTVER\"#" $JCPATH/etc/install.xml
-perl  -i'.orig' -p -e "s#<property\s+name=\"ctl.version\"\s+value=\".*?\"#<property name=\"ctl.version\" value=\"$CTLVER\"#" $JCPATH/etc/install.xml
-
-# update reportcenter version
-
-perl  -i'.orig' -p -e "s#^version\.number\s*=.*\$#version.number=$RCVER#" $RCPATH/version.properties
-perl  -i'.orig' -p -e "s#^app\.version=.*\$#app.version=$RCVER#" $RCPATH/application.properties
-perl  -i'.orig' -p -e "s#^reportcenter\.version\.num=.*\$#reportcenter.version.num=$RCVER#" $RCPATH/grails-app/i18n/messages.properties
-perl  -i'.orig' -p -e "s#<property\s+name=\"reportcenter.version\"\s+value=\".*?\"#<property name=\"reportcenter.version\" value=\"$RCVER\"#" $RCPATH/etc/install.xml
-perl  -i'.orig' -p -e "s#<property\s+name=\"commander.version\"\s+value=\".*?\"#<property name=\"commander.version\" value=\"$CTVER\"#" $RCPATH/etc/install.xml
-perl  -i'.orig' -p -e "s#<property\s+name=\"ctl.version\"\s+value=\".*?\"#<property name=\"ctl.version\" value=\"$CTLVER\"#" $RCPATH/etc/install.xml
+perl  -i'.orig' -p -e "s#^version\.number\s*=.*\$#version.number=$CTVER#" $CCPATH/version.properties
+perl  -i'.orig' -p -e "s#^app\.version=.*\$#app.version=$CTVER#" $CCPATH/application.properties
+perl  -i'.orig' -p -e "s#^ctlcenter\.version\.num=.*\$#ctlcenter.version.num=$CTVER#" $CCPATH/grails-app/i18n/messages.properties
+perl  -i'.orig' -p -e "s#<property\s+name=\"ctlcenter.version\"\s+value=\".*?\"#<property name=\"ctlcenter.version\" value=\"$CTVER\"#" $CCPATH/etc/install.xml
+perl  -i'.orig' -p -e "s#<property\s+name=\"commander.version\"\s+value=\".*?\"#<property name=\"commander.version\" value=\"$CTVER\"#" $CCPATH/etc/install.xml
+perl  -i'.orig' -p -e "s#<property\s+name=\"ctl.version\"\s+value=\".*?\"#<property name=\"ctl.version\" value=\"$CTLVER\"#" $CCPATH/etc/install.xml
 
 echo "updating project.xml"
 
 echo > $CTPATH/ctbuild/build.properties
-ant -Dctl.path=$CTLPATH -Dctier.path=$CTPATH -Djc.path=$JCPATH -f $CTPATH/ctbuild/build.xml
-perl  -i'.orig' -p -e "s#^CTLVERS=.*\$#CTLVERS=$CTLVER#" $CTPATH/buildall.sh
-perl  -i'.orig' -p -e "s#^CTIERVERS=.*\$#CTIERVERS=$CTVER#" $CTPATH/buildall.sh
-perl  -i'.orig' -p -e "s#^RCVERS=.*\$#RCVERS=$RCVER#" $CTPATH/buildall.sh
-perl  -i'.orig' -p -e "s#^JCVERS=.*\$#JCVERS=$JCVER#" $CTPATH/buildall.sh
+ant -Dctl.path=$CTLPATH -Dctier.path=$CTPATH  -f $CTPATH/ctbuild/build.xml
 
-
-if [ "$8" == "-commit" ] ; then
+if [ "$4" == "-commit" ] ; then
     svn commit -m "update version to $CTLVER, update dependencies" $CTLPATH/project.xml $CTLPATH/bundle/project.xml $CTLPATH/version.properties
-    svn commit -m "update version to $CTVER, update dependencies" $CTPATH/version.properties $CTPATH/common/project.xml $CTPATH/commander/project.xml $CTPATH/workbench/project.xml $CTPATH/installer/project.xml
-    svn commit -m "update version to $JCVER" $JCPATH/application.properties $JCPATH/grails-app/i18n/messages.properties $JCPATH/etc/install.xml $JCPATH/version.properties
-    svn commit -m "update version to $RCVER" $RCPATH/application.properties $RCPATH/grails-app/i18n/messages.properties $RCPATH/etc/install.xml $RCPATH/version.properties
-    svn commit -m "update project.xml definitions to latest" $CTPATH/ctbuild/objects/project.xml $CTPATH/ctbuild/build.properties $CTPATH/ctbuild/jobs/jobs.xml $CTPATH/buildall.sh
+    svn commit -m "update version to $CTVER, update dependencies" $CTPATH/version.properties $CTPATH/common/project.xml $CTPATH/commander/project.xml $CTPATH/workbench/project.xml $CTPATH/installer/project.xml \
+         $CCPATH/application.properties $CCPATH/grails-app/i18n/messages.properties $CCPATH/etc/install.xml $CCPATH/version.properties
+    svn commit -m "update project.xml definitions to latest"  $CTPATH/ctbuild/build.properties $CTPATH/ctbuild/jobs/jobs.xml $CTPATH/buildall.sh
 fi
--- tagrelease.sh
+++ tagrelease.sh
@@ -1,62 +1,50 @@
 #!/bin/sh
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ]  || [ -z "$7" ]  ; then
-    echo "usage: newrelease <ctl-version> <ctl-path> <ctier-version> <ct-path> <jc-version> <jc-path> <mf-path>  [-status | -commit]"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] ; then
+    echo "usage: newrelease <ctier-version> <ctl-path> <ct-path> <mf-path> [-status | -commit]"
     exit 1
 fi
 
 IDENT="release"
 
-
-CTLVER=$1
+CTVER=$1
+CTLVER=$CTVER
 CTLVERNAME=`echo $CTLVER | perl -pe 's/\./-/g'`
 CTLPATH=$2
-CTVER=$3
 CTVERNAME=`echo $CTVER | perl -pe 's/\./-/g'`
-CTPATH=$4
-JCVER=$5
-JCVERNAME=`echo $JCVER | perl -pe 's/\./-/g'`
-JCPATH=$6
+CTPATH=$3
 
 #moduleforge/controltier source path
-MFPATH=$7
+MFPATH=$4
 
 CTLURL=`svn info --xml --non-interactive $CTLPATH | grep url | perl -pe 's/<.+?>//g' ` 
 CTURL=`svn info --xml --non-interactive $CTPATH | grep url | perl -pe 's/<.+?>//g' ` 
-JCURL=`svn info --xml --non-interactive $JCPATH | grep url | perl -pe 's/<.+?>//g' | perl -pe 's{/branches/(.+?)/.*$}{/branches/\1}'` 
 MFURL=`svn info --xml --non-interactive $MFPATH | grep url | perl -pe 's/<.+?>//g' ` 
 
 CTLBRANCH=`echo $CTLURL | perl -pe 's#^.*?/branches/(.*?)/?$#\1#g'`
 CTBRANCH=`echo $CTURL | perl -pe 's#^.*?/branches/(.*?)/?$#\1#g'`
-JCBRANCH=`echo $JCURL | perl -pe 's#^.*?/branches/(.*?)/?$#\1#g'`
 MFBRANCH=`echo $MFURL | perl -pe 's#^.*?/branches/(.*?)/?$#\1#g'`
 
 CTLBASE=`echo $CTLURL | perl -pe 's#/branches/.*##g'`
 CTBASE=`echo $CTURL | perl -pe 's#/branches/.*##g'`
-JCBASE=`echo $JCURL | perl -pe 's#/branches/.*##g'`
 MFBASE=`echo $MFURL | perl -pe 's#/branches/.*##g'`
 
 CTLTAG="$CTLBASE/tags/ctl-dispatch-$CTLVERNAME-$IDENT"
 CTTAG="$CTBASE/tags/controltier-$CTVERNAME-$IDENT"
-JCTAG="$JCBASE/tags/jobcenter-$JCVERNAME-$IDENT"
 MFTAG="$MFBASE/tags/controltier-$CTVERNAME-$IDENT"
 
 
-if [ "$8" == "-commit" ] ; then
-    svn copy -m \""Tag $CTLVER release from $CTLBRANCH branch"\" $CTLURL $CTLTAG ;
-    svn copy -m \""Tag $CTVER release from $CTBRANCH branch"\" $CTURL $CTTAG ;
-    svn copy -m \""Tag $JCVER release from $JCBRANCH branch"\" $JCURL $JCTAG ;
-    svn copy -m \""Tag $CTVER release from $MFBRANCH branch"\" $MFURL $MFTAG ;
+if [ "$5" == "-commit" ] ; then
+    svn copy -m \""Tag $CTLVER $IDENT from $CTLBRANCH branch"\" $CTLURL $CTLTAG ;
+    svn copy -m \""Tag $CTVER $IDENT from $CTBRANCH branch"\" $CTURL $CTTAG ;
+    svn copy -m \""Tag $CTVER $IDENT from $MFBRANCH branch"\" $MFURL $MFTAG ;
     echo COMMITED TAGS ;
 else
-    echo svn copy -m \""Tag $CTLVER release from $CTLBRANCH branch"\" $CTLURL $CTLTAG ;
-    echo svn copy -m \""Tag $CTVER release from $CTBRANCH branch"\" $CTURL $CTTAG ;
-    echo svn copy -m \""Tag $JCVER release from $JCBRANCH branch"\" $JCURL $JCTAG ;
-    echo svn copy -m \""Tag $CTVER release from $MFBRANCH branch"\" $MFURL $MFTAG ;
+    echo svn copy -m \""Tag $CTLVER $IDENT from $CTLBRANCH branch"\" $CTLURL $CTLTAG ;
+    echo svn copy -m \""Tag $CTVER $IDENT from $CTBRANCH branch"\" $CTURL $CTTAG ;
+    echo svn copy -m \""Tag $CTVER $IDENT from $MFBRANCH branch"\" $MFURL $MFTAG ;
 
     echo "Use -commit to commit commit to execute these commands"
 fi
-
-#echo ant -Dctl.branchname=$CTLBRANCH -Dctl.path=$CTLPATH -Dctier.branchname=$CTBRANCH -Dctier.path=$CTPATH -Djc.branchname=$JCBRANCH -Djc.path=$JCPATH -Drc.branchname=$CTBRANCH -f $CTPATH/ctbuild/build.xml 
 
 
